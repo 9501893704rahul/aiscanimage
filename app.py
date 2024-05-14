09c7594d149669ddaa9986e605987c44a3f408c6
9501893704rahul <Rahulthakurhm@gmail.com>
2024-05-14 16:27:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,4 +117,5 @@ def extract():
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
